6853e3a1803e4275db5983c7070848feaa428fa1
Jhoabe Leão <jhoabe.leao@hotmail.com>
2022-04-11 10:25:36 -0300

    Loop While

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
     // Variável
     //$nome = 'Guilherme';
-    $idade = '26'
+    //$idade = '26'
     //$Guilherme = 'Bla Bla'
 
     //Concatenar variáveis
@@ -22,11 +22,24 @@
     // === IDÊNTICO  
     // !== DIFERENTE  
     // 26 inteiro '26'String 
-    if ($idade !== 26){
-        echo 'Verdadeiro';
-    }else {
-        echo 'Falso';
-    }
+   // if ($idade !== 26){
+     //   echo 'Verdadeiro';
+   // }else {
+       // echo 'Falso';
+  //  }
 
+    /* Laço FOR
+    for ($i = 0; $i < 10; $i++){
+        echo $i;
+        echo '<hr>'
+    }*/
+
+    /*Loop While
+    $i = 0;
+    while($i < 10) {
+        echo $i;
+        echo '<br>';
+        $i++;
+    }*/
 
 ?>    
