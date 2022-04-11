defc916017fe8536cc4ee8449a9b3979038c7b9d
Jhoabe Leão <jhoabe.leao@hotmail.com>
2022-04-11 10:07:37 -0300

    Comentários

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+    // PHP = Personal Home Page
+    // Variáveis
+    // Variável de variável
+    // Concatenar
+    // Comparar Valores
+    // >= <= == !=
+    // === !==
+    // Looping
+    // Funções
+    // Classes
+
+
+?>    
